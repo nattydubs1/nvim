deec7eaf30cd0fdfa002c82a35990fac2c3c94ea
natty_dubs1 <96153503+nattydubs1@users.noreply.github.com>
2026-02-18 10:24:53 -0500

    Configure gitsigns and replace vim-fugitive with Neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,11 +1,64 @@
 -- lua/plugins/git.lua
 return {
+  -- Gutter signs, blame, hunk staging
   {
     "lewis6991/gitsigns.nvim",
-    config = true,
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add          = { text = "▎" },
+          change       = { text = "▎" },
+          delete       = { text = "" },
+          topdelete    = { text = "" },
+          changedelete = { text = "▎" },
+        },
+        current_line_blame = true, -- inline blame, unobtrusive
+        current_line_blame_opts = {
+          delay = 500,
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          local map = function(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+          end
+
+          -- Navigation
+          map("n", "]h", gs.next_hunk, "Next hunk")
+          map("n", "[h", gs.prev_hunk, "Prev hunk")
+
+          -- Staging
+          map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+          map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+          map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
+          map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
+
+          -- Viewing
+          map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
+          map("n", "<leader>hb", gs.blame_line, "Blame line")
+          map("n", "<leader>hd", gs.diffthis, "Diff this")
+        end,
+      })
+    end,
   },
+
+  -- Magit-style interface
   {
-    "tpope/vim-fugitive",
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim", -- much better diff view
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+    },
+    config = function()
+      require("neogit").setup({
+        integrations = {
+          diffview = true,
+          telescope = true,
+        },
+      })
+    end,
   },
 }
-
