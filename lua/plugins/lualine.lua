c14d2e3ee53d3d4ef7e32975e1f7d928d678500b
natty_dubs1 <96153503+nattydubs1@users.noreply.github.com>
2026-02-21 11:13:51 -0500

    Modify lualine setup with new theme and options
    Updated lualine configuration to use 'dracula-nvim' theme and added diagnostics to the status line.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,22 +1,25 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "dracula",
-          globalstatus = true,
-          section_separators = { left = "", right = "" },
-          component_separators = { left = "", right = "" },
-        },
-        sections = {
-          lualine_a = { { "mode", upper = true } },
-          lualine_b = { "branch", "diff" },
-          lualine_c = { "filename" },
-          lualine_x = { "encoding", "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
-        },
-      })
-    end,
-  }
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula-nvim",
+				icons_enabled = true,
+				globalstatus = true,
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				always_divide_middle = true,
+			},
+			sections = {
+				lualine_a = { { "mode", upper = true } },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
+}
+
